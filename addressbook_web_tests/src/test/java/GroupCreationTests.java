e248774c0562d0d57dae906a42d524579ec04f33
Alexei Barantsev <barancev@gmail.com>
2023-07-05 19:30:37 +0300

    Реализован модельный класс для представления групп

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -5,12 +6,20 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void canCreateGroup() {
         openGroupsPage();
-        createGroup("group name", "group header", "group footer");
+        createGroup(new GroupData("group name", "group header", "group footer"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup = new GroupData();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
     }
 }
