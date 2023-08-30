3532bfa1db30a197397e9a35290ea93adf186e9d
Alexei Barantsev <barancev@gmail.com>
2023-08-30 11:57:33 +0300

    Реализована генерация тестовых данных с сохранением в файл и загрузка тестовых данных из файла

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.addressbook.tests;
 
 import org.junit.jupiter.api.Test;
+import ru.stqa.addressbook.common.CommonFunctions;
 import ru.stqa.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
@@ -8,8 +9,8 @@ public class ContactCreationTests extends TestBase {
     @Test
     void canCreateContact() {
         var contact = new ContactData()
-                .withFirstname(randomString(10))
-                .withLastname(randomString(10))
+                .withFirstname(CommonFunctions.randomString(10))
+                .withLastname(CommonFunctions.randomString(10))
                 .withPhoto(randomFile("src/test/resources/images"));
         app.contacts().create(contact);
     }
