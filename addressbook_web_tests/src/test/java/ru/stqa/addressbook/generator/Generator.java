8bf4e629227fd954efb35e6fb4d5ddb0d4328671
Alexei Barantsev <barancev@gmail.com>
2023-10-04 10:21:17 +0300

    Генерация тестовых данных переписана с использованием функционального стиля

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
@@ -7,12 +7,15 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import ru.stqa.addressbook.common.CommonFunctions;
+import ru.stqa.addressbook.model.ContactData;
 import ru.stqa.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Generator {
 
@@ -52,19 +55,21 @@ public class Generator {
         }
     }
 
+    private Object generateData(Supplier<Object> dataSupplier) {
+        return Stream.generate(dataSupplier).limit(count).collect(Collectors.toList());
+    }
+
     private Object generateGroups() {
-        var result = new ArrayList<GroupData>();
-        for (int i = 0; i < count; i++) {
-            result.add(new GroupData()
-                    .withName(CommonFunctions.randomString(i * 10))
-                    .withHeader(CommonFunctions.randomString(i * 10))
-                    .withFooter(CommonFunctions.randomString(i * 10)));
-        }
-        return result;
+        return generateData(() -> new GroupData()
+                .withName(CommonFunctions.randomString(10))
+                .withHeader(CommonFunctions.randomString(10))
+                .withFooter(CommonFunctions.randomString(10)));
     }
 
     private Object generateContacts() {
-        return null;
+        return generateData(() -> new ContactData()
+                .withFirstname(CommonFunctions.randomString(10))
+                .withLastname(CommonFunctions.randomString(10)));
     }
 
     private void save(Object data) throws IOException {
--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -3,22 +3,17 @@ package ru.stqa.geometry;
 import ru.stqa.geometry.figures.Square;
 
 import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Geometry {
     public static void main(String[] args) {
-        var squares = List.of(new Square(7.0), new Square(5.0), new Square(3.0));
-//        for (Square square : squares) {
-//            Square.printSquareArea(square);
-//        }
-        Consumer<Square> print = square -> {
-            Square.printArea(square);
-            Square.printPerimeter(square);
-        };
-        squares.forEach(print);
+        Supplier<Square> randomSquare = () -> new Square(new Random().nextDouble(100.0));
+        var squares = Stream.generate(randomSquare).limit(5);
 
-//        Rectangle.printRectangleArea(3.0, 5.0);
-//        Rectangle.printRectangleArea(7.0, 9.0);
+        squares.peek(Square::printArea).forEach(Square::printPerimeter);
     }
 
 }
