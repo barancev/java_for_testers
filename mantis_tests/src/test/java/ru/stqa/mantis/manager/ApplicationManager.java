a4ae69a8427c3406933659c24d847d8cce612735
Alexei Barantsev <barancev@gmail.com>
2023-11-08 17:03:14 +0300

    Реализовано взаимодействие с Mantis через REST API

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
     private UserHelper userHelper;
     private JamesApiHelper jamesApiHelper;
     private DeveloperMailHelper developerMailHelper;
+    private RestApiHelper restApiHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -88,6 +89,13 @@ public class ApplicationManager {
             developerMailHelper = new DeveloperMailHelper(this);
         }
         return developerMailHelper;
+    }
+
+    public RestApiHelper rest() {
+        if (restApiHelper == null) {
+            restApiHelper = new RestApiHelper(this);
+        }
+        return restApiHelper;
     }
 
     public String property(String name) {
